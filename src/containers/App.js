7c9591d245cf3989ed5e422599c9ebe7a1bd8625
minseo-hong <mshong1007@gmail.com>
2021-12-06 18:46:11 +0900

    App의 componentDidMount 메소드를 React Hooks 방식으로 변경

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import ErrorBoundary from "./ErrorBoundary";
@@ -21,6 +21,12 @@ function App() {
   //     .then((response) => response.json())
   //     .then((users) => this.setState({ robots: users }));
   // }
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setRobots(users));
+  }, []);
 
   const onSearchChange = (event) => {
     setSearchField(event.target.value);
